32fb17d99ffdfbdbc4d40d9a24eb2d73c4516c4e
Junying Li <jesselix@live.com>
2025-07-06 23:41:52 +0800

    +

---
+++ emall-account/src/main/java/li/jesse/emallaccount/EmallAccountApplication.java
@@ -0,0 +1,13 @@
+package li.jesse.emallaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmallAccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmallAccountApplication.class, args);
+    }
+
+}
---
+++ emall-account/src/test/java/li/jesse/emallaccount/EmallAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package li.jesse.emallaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmallAccountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
